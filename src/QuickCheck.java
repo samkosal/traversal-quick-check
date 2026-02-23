b1c374eb253880dff6b50b2b114d64167d062f26
samkosal <67755908+samkosal@users.noreply.github.com>
2026-02-22 19:11:11 -0800

    finished

--- src/QuickCheck.java
+++ src/QuickCheck.java
@@ -15,6 +15,17 @@ public class QuickCheck {
    * @param node the root node of the binary tree
    */
   public static void printLongerThan7(TreeNode<String> node) {
+    if (node == null) {
+      return;
+    }
+
+    if ((node.value).length() > 7) {
+      System.out.println(node.value);
+    }
+
+    printLongerThan7(node.left);
+    printLongerThan7(node.right);
+
   }
 
   /**
@@ -28,6 +39,18 @@ public class QuickCheck {
    * @return the sum of all odd-valued nodes
    */
   public static int oddSum(TreeNode<Integer> node) {
-      return -1;
+    if (node == null) {
+      return 0;
+    }
+
+    int left = oddSum(node.left);
+    int right = oddSum(node.right);
+
+    if (node.value % 2 != 0) {
+      return left + right + node.value;
+    }
+    return left + right;
+   
+
   }
 }
